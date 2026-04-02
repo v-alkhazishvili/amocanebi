00bce046d8e7ca5d3db63258c3ad04af13d08039
v-alkhazishvili <58003187+v-alkhazishvili@users.noreply.github.com>
2026-04-02 13:17:23 +0400

    Update Program.cs

--- calculatorAPI/Program.cs
+++ calculatorAPI/Program.cs
@@ -5,8 +5,7 @@ using CalculatorApi.Services;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-// Simple endpoint that echoes back the string you send
-app.MapGet("/",  ([FromBody] string text)  =>
+app.MapGet("/",  (string text)  => //localhost:5130/?text=2+(5*2)
 {
     if (string.IsNullOrWhiteSpace(text))
     {
@@ -14,7 +13,10 @@ app.MapGet("/",  ([FromBody] string text)  =>
     }
     var calculatorService = new CalculatorService();
     var result = calculatorService.Evaluate(text);    
-    return Results.Ok(result);
-});            
-// Optional: GET endpoint to test if API is running
+        
+    return Results.Ok(new CalculationResult
+    {
+        Result = result
+    });
+});                    
 app.Run();
